7d2cec971b2e2dc7c0457f67779f693706ea355e
serii burduja <seriiburduja@gmail.com>
2024-12-01 12:18:25 +0200

    feat: root path

--- modules/gitSsh.py
+++ modules/gitSsh.py
@@ -4,6 +4,7 @@ from rich import print
 def gitSsh():
     os.system("sudo pacman -S openssh")
     ## run script ../scripts/git-ssh.sh
-    current_script_path = os.path.dirname(os.path.realpath(__file__))
-    print(f"current_script_path: {current_script_path}")
-    os.system(f"bash {current_script_path}/scripts/git-ssh.sh")
+    root_scripts_path = os.path.dirname(os.path.abspath(__file__))
+    print(f"root_scripts_path: {root_scripts_path}")
+
+    # os.system(f"bash ../scripts/{current_script_path}/scripts/git-ssh.sh")
