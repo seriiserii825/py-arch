9e0c6d037a880ed41a388f2c224854e7fef2bb81
serii burduja <seriiburduja@gmail.com>
2024-12-01 11:38:34 +0200

    feat: readme and pip.sh

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
-# py-arch
+## Before archinstall
+
+```
+lsblk
+cfdisk /dev/sda
+delete all partitions
+
+in /etc/pacman.conf uncomment #Color and change Paralel to 5
+```
+
+## connect to wifi
+
+```
+rfkill list
+rfkill unblock wifi
+ip link
+ip link set wlan0 up
+iwctl
+device list
+station wlan0 scan
+station wlan0 get-networks
+station wlan0 connect SSID(wait 5sec before quit)
+quit
+ping ya.ru
+```
+
+## archinstall
+
+```
+set disk configuration
+choos networkmanager
+```
+
+## After archinstall and before reboot
+
+```
+sudo pacman -S linux-lts linux-lts-headers
+old - sudo pacman -S linux-headers
+
+sudo pacman -S broadcom-wl networkmanager
+sudo systemctl enable NetworkManager
+```
+
+## After reboot
+
+```
+nmcli device wifi list
+nmcli device wifi connect SSID password PASSWORD
+```
+
+## start script
+```
+create dir Documents
+and clone this repo
+~/Documents/bash-arch
+cd ~/Documents/bash-arch
+chmod +x arch.sh
+./arch.sh
+```
---
+++ pip.sh
@@ -0,0 +1,71 @@
+#! /bin/bash
+
+function init(){
+  current_dir=$(pwd)
+  python3 -m venv venv
+  source venv/bin/activate
+  python3 -m pip install --upgrade pip
+}
+
+function activate(){
+  source venv/bin/activate
+}
+
+function initIfNotExists(){
+  if [ ! -d "venv" ]; then
+    init
+  else
+    activate
+  fi
+}
+
+function menu(){
+  echo "${tgreen}1. List${treset}"
+  echo "${tblue}2. Install Package${treset}"
+  echo "${tblue}3. Install all${treset}"
+  echo "${tmagenta}4. Uninstall${treset}"
+  echo "${tmagenta}5. Exit${treset}"
+
+  read -p "Enter the option: " option
+  case $option in
+    1)
+      initIfNotExists
+      bat requirements.txt
+      deactivate
+      menu
+      ;;
+    2)
+      read -p "Enter the package name: " package_name
+      initIfNotExists
+      python3 -m pip install $package_name
+      pip freeze > requirements.txt
+      deactivate
+      menu
+      ;;
+    3)
+      initIfNotExists
+      python3 -m pip install -r requirements.txt
+      deactivate
+      menu
+      ;;
+    4)
+      initIfNotExists
+      # package name from file requirements.txt with fzf
+      package_name=$(cat requirements.txt | fzf)
+      package_name=$(echo $package_name | cut -d'=' -f1)
+      python3 -m pip uninstall $package_name
+      pip freeze > requirements.txt
+      deactivate
+      menu
+      ;;
+    5)
+      exit 0
+      ;;
+    *)
+      echo "Invalid option"
+      exit 0
+      ;;
+  esac
+}
+
+menu
