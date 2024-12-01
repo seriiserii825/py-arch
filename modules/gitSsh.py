3d630971c6654736b714ca723dda8a1e34da65de
serii burduja <seriiburduja@gmail.com>
2024-12-01 12:16:53 +0200

    feat: current script path

--- modules/gitSsh.py
+++ modules/gitSsh.py
@@ -3,6 +3,7 @@ import os
 from rich import print
 def gitSsh():
     os.system("sudo pacman -S openssh")
-    ## run script scripts/git-ssh.sh
+    ## run script ../scripts/git-ssh.sh
     current_script_path = os.path.dirname(os.path.realpath(__file__))
+    print(f"current_script_path: {current_script_path}")
     os.system(f"bash {current_script_path}/scripts/git-ssh.sh")
