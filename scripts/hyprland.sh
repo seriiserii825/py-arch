07341a0ff135c1701a652ce92cb3614d595f5bf4
serii burduja <seriiburduja@gmail.com>
2024-12-28 10:12:29 +0200

    feat: hyprlock

--- scripts/hyprland.sh
+++ scripts/hyprland.sh
@@ -7,18 +7,48 @@ function installAll(){
 function symlinkAll(){
   cd ~/.config
 
-  rm -rf hypr
   rm -rf swaylock
   rm -rf waybar
   rm -rf wlogout
+  rm -rf wofi
+  rm hypr/hyprlock.conf
 
-  ln -s ~/xubuntu/hyprland/hypr
-  ln -s ~/xubuntu/hyprland/swaylock
+  ln -s ~/xubuntu/hyprland/hypr/hyprlock.conf hypr/hyprlock.conf
   ln -s ~/xubuntu/hyprland/waybar
   ln -s ~/xubuntu/hyprland/wlogout
+  ln -s ~/xubuntu/hyprland/wofi
 }
 
-installAll
+function setupMonitors(){
+  hyprctl monitors
+}
+
+function installFonts(){
+  sudo pacman -S ttf-font-awesome otf-font-awesome ttf-jetbrains-mono
+}
+select action in installAll symlinkAll setupMonitors Exit
+do
+  case $action in
+    installAll)
+      installAll
+      ;;
+    symlinkAll)
+      symlinkAll
+      ;;
+    setupMonitors)
+      setupMonitors
+      ;;
+    installFonts)
+      installFonts
+      ;;
+    Exit)
+      break
+      ;;
+  esac
+done
+
+
+# installAll
 
 # mkdir -p ~/.config/wlogout
 # sudo cp /etc/wlogout/layout ~/.config/wlogout/
