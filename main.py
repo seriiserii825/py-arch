514a4c9c4db5bfb9658c208b296aedfbe6572b05
serii burduja <seriiburduja@gmail.com>
2025-01-25 09:38:47 +0200

    feat: changed index

--- main.py
+++ main.py
@@ -102,85 +102,85 @@ def menu():
         print("[blue]Speed up yay")
         speedUpYay()
     elif menu_entry_indices == 3:
+        print("[blue]You choose to Apps.")
+        os.system(f"bash {current_script_path}/scripts/apps.sh")
+    elif menu_entry_indices == 4:
         print("[blue]You choose to Install i3.")
         installI3()
-    elif menu_entry_indices == 4:
+    elif menu_entry_indices == 5:
         print("[blue]You choose to Nvidia.")
         nvidia()
-    elif menu_entry_indices == 5:
+    elif menu_entry_indices == 6:
         print("[blue]You choose to Chrome.")
         chrome()
-    elif menu_entry_indices == 6:
+    elif menu_entry_indices == 7:
         print("[blue]You choose to Git ssh.")
         gitSsh()
-    elif menu_entry_indices == 7:
+    elif menu_entry_indices == 9:
         print("[blue]You choose to Download xubuntu from git.")
         os.system(f"bash {current_script_path}/scripts/xubuntu.sh")
-    elif menu_entry_indices == 8:
+    elif menu_entry_indices == 10:
         print("[blue]You choose to Zsh Oh-my-zsh.")
         os.system(f"bash {current_script_path}/scripts/zsh.sh")
-    elif menu_entry_indices == 9:
+    elif menu_entry_indices == 11:
         print("[blue]You choose to nvm(node js).")
         print(f"current_script_path: {current_script_path}")
         os.system(f"bash {current_script_path}/scripts/nvm.sh")
-    elif menu_entry_indices == 10:
+    elif menu_entry_indices == 12:
         print("[blue]You choose to Neovim.")
         os.system(f"bash {current_script_path}/scripts/nvim.sh")
-    elif menu_entry_indices == 11:
+    elif menu_entry_indices == 13:
         print("[blue]You choose to Nerd Fonts.")
         os.system(f"bash {current_script_path}/scripts/nerd-fonts.sh")
-    elif menu_entry_indices == 12:
+    elif menu_entry_indices == 14:
         print("[blue]You choose to Locale.")
         os.system(f"bash {current_script_path}/scripts/locale.sh")
-    elif menu_entry_indices == 13:
+    elif menu_entry_indices == 15:
         print("[blue]You choose to Bash repos.")
         os.system(f"bash {current_script_path}/scripts/bash-repos.sh")
-    elif menu_entry_indices == 14:
+    elif menu_entry_indices == 16:
         print("[blue]You choose to Gpg Pass.")
         os.system(f"bash {current_script_path}/scripts/gpg.sh")
-    elif menu_entry_indices == 15:
+    elif menu_entry_indices == 17:
         print("[blue]You choose to Py repos.")
         os.system(f"bash {current_script_path}/scripts/py-repos.sh")
-    elif menu_entry_indices == 16:
+    elif menu_entry_indices == 18:
         print("[blue]You choose to Audio.")
         os.system(f"bash {current_script_path}/scripts/audio.sh")
-    elif menu_entry_indices == 17:
+    elif menu_entry_indices == 19:
         print("[blue]You choose to Bluetooth.")
         os.system(f"bash {current_script_path}/scripts/bluetooth.sh")
-    elif menu_entry_indices == 18:
+    elif menu_entry_indices == 20:
         print("[blue]You choose to BrowserPass.")
         os.system(f"bash {current_script_path}/scripts/browserpass.sh")
-    elif menu_entry_indices == 19:
-        print("[blue]You choose to Apps.")
-        os.system(f"bash {current_script_path}/scripts/apps.sh")
-    elif menu_entry_indices == 20:
+    elif menu_entry_indices == 21:
         print("[blue]You choose to Chrome extenstions.")
         os.system(f"bash {current_script_path}/scripts/chrome-extensions.sh")
-    elif menu_entry_indices == 21:
+    elif menu_entry_indices == 22:
         print("[blue]You choose to Local wp.")
         os.system(f"bash {current_script_path}/scripts/local-by-flywheel.sh")
-    elif menu_entry_indices == 22:
+    elif menu_entry_indices == 23:
         print("[blue]You choose to Libre Office.")
         os.system(f"bash {current_script_path}/scripts/libre-office.sh")
-    elif menu_entry_indices == 23:
+    elif menu_entry_indices == 24:
         print("[blue]You choose to Smtp.")
         os.system(f"bash {current_script_path}/scripts/smtp.sh")
-    elif menu_entry_indices == 24:
+    elif menu_entry_indices == 25:
         print("[blue]You choose to Docker.")
         os.system(f"bash {current_script_path}/scripts/docker.sh")
-    elif menu_entry_indices == 25:
+    elif menu_entry_indices == 26:
         print("[blue]You choose to Docker Compose.")
         os.system(f"bash {current_script_path}/scripts/docker-compose.sh")
-    elif menu_entry_indices == 26:
+    elif menu_entry_indices == 27:
         print("[blue]You choose to Youtube.")
         os.system(f"bash {current_script_path}/scripts/youtube.sh")
-    elif menu_entry_indices == 27:
+    elif menu_entry_indices == 28:
         print("[blue]You choose to Hyprland.")
         os.system(f"bash {current_script_path}/scripts/hyprland.sh")
-    elif menu_entry_indices == 28:
+    elif menu_entry_indices == 29:
         print("[blue]You choose to Android.")
         os.system(f"bash {current_script_path}/scripts/android.sh")
-    elif menu_entry_indices == 29:
+    elif menu_entry_indices == 30:
         print("[blue]Emojji.")
         os.system(f"bash {current_script_path}/scripts/emojji.sh")
         
