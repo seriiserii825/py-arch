071a17fbc2518b4dcefc28f79810796b7a9445d9
serii burduja <seriiburduja@gmail.com>
2025-01-25 10:39:05 +0200

    neovim added message to install nvm

--- scripts/nvim.sh
+++ scripts/nvim.sh
@@ -1,3 +1,14 @@
+read -p "Do you already installed nvm, npm and yarn? (y/n) " -n 1 -r
+
+if [[ $REPLY =~ ^[Nn]$ ]]
+then
+    echo "Please install nvm, npm and yarn first."
+    echo "nvm install 18"
+    echo "nvm use 18"
+    echo "npm install -g yarn"
+    exit 1
+fi
+
 sudo pacman -S neovim
 mkdir ~/Documents/Apps
 git clone git@github.com:seriiserii825/nvim-coc.git ~/Documents/Apps/nvim
