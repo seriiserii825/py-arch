cf8d7542090cabe16f375e21c9a63d8698ffe239
serii burduja <seriiburduja@gmail.com>
2025-01-25 09:53:14 +0200

    feat: chrome update

--- scripts/apps.sh
+++ scripts/apps.sh
@@ -3,6 +3,6 @@ sudo pacman -S  mousepad libnotify alacritty copyq openssh xclip xsel jq
 sudo pacman -S bat wget curl gnome-keyring unzip zip linux-firmware libmagick jpegoptim
 sudo pacman -S git lazygit
 sudo pacman -S telegram-desktop flameshot php tesseract tesseract-data-eng translate-shell
-sudo pacman -S google-chrome-stable
+sudo pacman -S google-chrome
 yay -S skypeforlinux-bin st
 yay -S chromedriver
