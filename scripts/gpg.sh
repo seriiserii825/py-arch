5a7727c0930775f20bf6b2cedf6f66e73be0b0ee
serii burduja <seriiburduja@gmail.com>
2025-02-07 08:25:12 +0200

    feat: udpate gpg.sh

--- scripts/gpg.sh
+++ scripts/gpg.sh
@@ -1,4 +1,6 @@
-zip_file_path=/home/serii/Downloads/desene_animate.zip
+current_user=$(whoami)
+
+zip_file_path=/home/$current_user/Downloads/desene_animate.zip
 
 if [ ! -f $zip_file_path ]; then
     echo "${tmagenta}$zip_file_path does not exist${treset}"
@@ -10,16 +12,22 @@ sudo pacman -S pass unzip zip
 cd ~/Downloads
 
 
-pass init serii
+pass init $current_user
 
-unzip $zip_file_path -d /home/serii/Downloads/desene_animate
+unzip $zip_file_path -d /home/$current_user/Downloads/desene_animate
 
 cd desene_animate
 
 gpg --import private.key
 gpg --import public.key
 
-git clone git@bitbucket.org:seriiserii825/passwords.git ~/Downloads/passwords
+read -p "Insert git repo passwords, like git@bitbucket.org:seriiserii825/passwords.git" git_repo
+
+if [ -z $git_repo ]; then
+    git_repo=git@bitbucket.org:seriiserii825/passwords.git
+fi
+
+git clone $git_repo ~/Downloads/passwords
 
 cd ~/.password-store
 rm * -rf
