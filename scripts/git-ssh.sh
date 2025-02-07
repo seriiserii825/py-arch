f4091039f22dd0a0f5333604bf178267b0b06de5
serii burduja <seriiburduja@gmail.com>
2025-02-07 08:21:05 +0200

    upd: update git

--- scripts/git-ssh.sh
+++ scripts/git-ssh.sh
@@ -1,14 +1,18 @@
-read -p "${tmagenta}Start copyq, configure and after script end, will have sshkey in clipboard${tmagena}" -n 1 -r
+read -p "${tmagenta}Start copyq, configure and after script end, will have sshkey in clipboard${treset}" -n 1 -r
 
 echo "${tblue}Installing ssh git${treset}"
 
-ssh-keygen -t rsa -b 4096 -C "seriiburduja@mail.com"
+read -p "${tgreen}Enter email for git${treset}" email
+read -p "${tblue}Enter name for git${treset}" name
+
+
+ssh-keygen -t rsa -b 4096 -C "$email"
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_rsa
 
 xclip -sel clip < ~/.ssh/id_rsa.pub
 
-git config --global user.email "seriiburduja@gmail.com"
-git config --global user.name "Serii"
+git config --global user.email "$email"
+git config --global user.name "$name"
 
 echo "${tgreen}Public key copied to clipboard, add it to github and bitbucket${treset}"
