f06f9baff9b205eaa8fdb653130e3a575243f19e
serii burduja <seriiburduja@gmail.com>
2025-06-21 06:39:39 +0300

    feat: apps

--- scripts/apps.sh
+++ scripts/apps.sh
@@ -1,4 +1,4 @@
-sudo pacman -S ttf-dejavu awesome-terminal-fonts woff2 dosfstools mtools zoxide
+sudo pacman -S ttf-dejavu awesome-terminal-fonts woff2 dosfstools mtools 
 sudo pacman -S  mousepad libnotify alacritty copyq openssh xclip xsel jq
 sudo pacman -S bat wget curl gnome-keyring unzip zip linux-firmware libmagick jpegoptim
 sudo pacman -S git lazygit
@@ -7,8 +7,9 @@ sudo pacman -S google-chrome
 sudo pacman -S tldr entr bashtop atuin lsd
 sudo pacman -S libreoffice-fresh 
 ## for docker up in old notebook
-sudo pacman -S libxcrypt-compat
-yay -S skypeforlinux-bin st
-yay -S google-chrome
-yay -S chromedriver
-yay -S pyenv pyenv-virtualenv
+sudo pacman -S libxcrypt-compat --noconfirm
+sudo pacman -S yazi ffmpeg 7zip poppler fd ripgrep fzf zoxide imagemagick --noconfirm
+yay -S resvg --noconfirm
+yay -S google-chrome-stable-bin --noconfirm
+yay -S chromedriver --noconfirm
+yay -S pyenv pyenv-virtualenv --noconfirm
