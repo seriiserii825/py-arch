0a4d6d594ff9812bc931fd26f15a3eb031e85398
serii burduja <seriiburduja@gmail.com>
2025-06-25 16:54:58 +0300

    feat: update zsh

--- scripts/zsh.sh
+++ scripts/zsh.sh
@@ -1,14 +1,13 @@
 sudo pacman -S zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 rm ~/.zshrc
-ln -s ~/xubuntu/.zshrc ~/.zshrc
-rm -rf ~/.config/alacritty
-git clone https://github.com/agkozak/zsh-z ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-z
+ln -s ~/dotfiles/.zshrc ~/.zshrc
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/MichaelAquilina/zsh-you-should-use.git $ZSH_CUSTOM/plugins/you-should-use
-git clone https://github.com/MichaelAquilina/zsh-you-should-use.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/you-should-use
-sudo pacman -S exa ripgrep universal-ctags fd the_silver_searcher python-pynvim fzf ack
+git clone https://github.com/zsh-users/zsh-completions.git \
+  ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
+
+sudo pacman -S exa ripgrep universal-ctags fd the_silver_searcher python-pynvim fzf ack zoxide
 
 sudo reboot now
