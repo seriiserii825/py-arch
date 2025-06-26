0b7b4ea74502bb05b3dc331856a0668fd4c8961c
serii <seriiburduja@gmail.com>
2025-06-26 21:59:33 +0300

    feat: zsh git opne

--- scripts/zsh.sh
+++ scripts/zsh.sh
@@ -5,6 +5,7 @@ ln -s ~/dotfiles/.zshrc ~/.zshrc
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/MichaelAquilina/zsh-you-should-use.git $ZSH_CUSTOM/plugins/you-should-use
+git clone https://github.com/paulirish/git-open.git $ZSH_CUSTOM/plugins/git-open
 git clone https://github.com/zsh-users/zsh-completions.git \
   ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
 git clone https://github.com/z-shell/F-Sy-H.git \
