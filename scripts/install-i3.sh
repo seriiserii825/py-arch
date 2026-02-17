a860283a9b6757147d715e3fe645e9db71501bf8
seriiserii825 <seriiburduja@gmail.com>
2026-02-17 20:31:41 +0200

    feat: install i3 rofi

--- scripts/install-i3.sh
+++ scripts/install-i3.sh
@@ -3,7 +3,7 @@ sudo touch /etc/pip.conf
 sudo echo "[global]\nbreak-system-packages = true" >> /etc/pip.conf
 
 mkdir Downloads Documents Picture
-sudo pacman -S xorg xorg-xinit rofi i3 polybar polkit dunst lxsession xorg-apps dmenu gnome-keyring polkit-gnome nitrogen feh polybar
+sudo pacman -S xorg xorg-xinit rofi i3 polybar polkit dunst lxsession xorg-apps dmenu gnome-keyring polkit-gnome nitrogen feh rofi 
 cd
 cp /etc/X11/xinitrc > ~/.xinitrc
 cat <<TEST >> ".xinitrc"
